fc1f4f9ae5205a0409344670a562742b4a60bc18
Fernando Aquistapace <fernando.akistapace@gmail.com>
2022-12-01 01:38:12 -0300

    .

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
-import listRings from "./list-rings";
-import {modal} from "./modals";
+// import listRings from "./list-rings";
+// import {modal} from "./modals";
 import {header} from "./header";
 import { resizer } from "./resizer";
+import { handlePPIRule } from "./ruler";
 
 // import {camera} from "./camera";
 
@@ -9,6 +10,7 @@ import { resizer } from "./resizer";
     // listRings.init();
     header.init()
     resizer.init()
+    handlePPIRule.init();
     // camera.init()
     window.addEventListener('DOMContentLoaded', ()=>{
         // modal.init();
--- src/js/resizer.js
+++ src/js/resizer.js
@@ -1,30 +1,16 @@
 import rings from "../data/rings.json";
-import toPx from "./to-px";
-
-const remCalc = (px, base = 20) => {
-    let tempPx = px
-    if (typeof px === 'string' || px instanceof String)
-      tempPx = tempPx.replace('px', '')
-  
-    tempPx = parseInt(tempPx)
-    return (1 / base) * tempPx + 'em'
-}
 
 const runner = (index)=> {  
     let value = rings[index];
-    let circle = document.querySelector('[data-modal-sizer] #image');
+    let circle = document.querySelector('[data-modal-sizer] [data-ring]');
     let ringSize = document.querySelector('[data-modal-sizer] .ringsize');
     let tableSize = document.querySelector('[data-modal-sizer] [data-size]');
     let tableMm = document.querySelector('[data-modal-sizer] [data-mm]');
     let tableCirc = document.querySelector('[data-modal-sizer] [data-circ]');
+    let size = (value.mm * 0.1).toFixed(2) * window.ppcm + 'px' 
 
-    // let size = (value.mm * 3.779528).toFixed(2)
-    let size = (value.mm * 0.1).toFixed(2)
-
-    
-    // circle.style.fontSize = size * window.ppcm +'px'
-    circle.style.width   = (size * window.ppcm)  +  'px' ;
-    circle.style.height  = (size * window.ppcm)  +  'px';
+    circle.style.width  = size ;
+    circle.style.height = size;
     
     ringSize.textContent = value.size;
     tableSize.textContent = value.size;
@@ -36,74 +22,119 @@ export const resizer = {
     sliderBar: ()=> document.querySelector(`[data-resizer] .slider`),
     setMin: rings[0].size,
     setMax: rings.length - 1,
-    startValue: 23,
+    startValue: 14,
     setDefaultValue() { runner(this.startValue) },
     rangebar() {
         const _this = this
-        // console.log(rings[0]);
         const slider = _this.sliderBar()
+        slider.value=this.startValue
         slider.oninput = function() {
             slider.max = _this.setMax;
             slider.min = _this.setMin;
             runner(this.value);
         }
-    },
 
-    modalContainer: ()=> document.querySelector('[data-modal-sizer]') ,
-    open() {
-        let modal = this.modalContainer()
-        let cards = document.querySelectorAll('[data-open-resizer]') 
-        cards.forEach(card => {
-           card.addEventListener('click', ()=> {
-                modal.classList.add('--opened')
-                document.body.classList.add('--no-scroll')
-           }); 
-        });
-    },
-    close() {
-        let modal = this.modalContainer()
-        let buttonClose = modal.querySelector('.close')
-        buttonClose.addEventListener('click', ()=> {
-            modal.classList.remove('--opened')
-            document.body.classList.remove('--no-scroll')
+
+        let minus = document.querySelector('[data-resizer] .minus')
+        let plus = document.querySelector('[data-resizer] .plus')
+        plus.addEventListener('click', ()=> {
+            let input = document.querySelector('[data-resizer] input')
+            input.stepUp()
+            runner(input.value);
         })
-
-        modal.addEventListener('click', (e)=> {
-            e.stopPropagation();
-            if (e.target.classList.contains('modal')) {
-                modal.classList.remove('--opened')
-                document.body.classList.remove('--no-scroll')
+        minus.addEventListener('click', ()=> {
+            let input = document.querySelector('[data-resizer] input')
+            if (input.value > 0) {
+                input.stepDown()
+                runner(input.value);
             }
         })
 
+    },
+    modalSizer: ()=> document.querySelector('[data-modal-sizer]'),
+    modalRuler: ()=> document.querySelector('[data-modal-ruler]'),
+    open() {
+        let modalRuler = this.modalRuler()
+        let modalSize  = this.modalSizer()
+        let button = document.querySelector('[data-open-resizer]') 
+        let buttonNext = document.querySelector('[data-next]') 
+
+        button.addEventListener('click', ()=> {
+            modalRuler.classList.add('--opened')
+            document.body.classList.add('--no-scroll')
+        });
+
+        buttonNext.addEventListener('click', ()=> {
+            modalRuler.classList.remove('--opened') 
+            document.body.classList.add('--no-scroll')
+            document.querySelector('[data-popup]').classList.add('--show')
+            
+            this.popupAlert(modalSize);
+        })
+    },
+    popupAlert(modalSize) {
+        let getPopup = document.querySelector('[data-popup]')
+        let decline = getPopup.querySelector('[data-decline]')
+        let accept = getPopup.querySelector('[data-accept]')
+
+        decline.addEventListener('click', ()=> {
+            document.querySelector('[data-popup]').classList.remove('--show')
+            
+        });
+        accept.addEventListener('click', ()=> {
+            document.querySelector('[data-popup]').classList.remove('--show')
+            this.setDefaultValue();
+            modalSize.classList.add('--opened')
+        });
+    },
+    close() {
+        let modalRuler = this.modalRuler();
+        let modalSize  = this.modalSizer();
+
+        modalSize.querySelector('.close')
+        .addEventListener('click', ()=> {
+            modalSize.classList.remove('--opened')
+            document.body.classList.remove('--no-scroll')
+        });
+
+        modalRuler.querySelector('.close')
+        .addEventListener('click', ()=> {
+            modalRuler.classList.remove('--opened')
+            document.body.classList.remove('--no-scroll')
+        });
+
+        modalSize.addEventListener('click', (e)=> {
+            e.stopPropagation();
+            if (e.target.classList.contains('modal')) {
+                modalSize.classList.remove('--opened')
+                document.body.classList.remove('--no-scroll')
+            }
+        });
+
+        modalRuler.addEventListener('click', (e)=> {
+            e.stopPropagation();
+            if (e.target.classList.contains('modal')) {
+                modalRuler.classList.remove('--opened')
+                document.body.classList.remove('--no-scroll')
+            }
+        });
+
         window.addEventListener('keyup', (e)=> {
             e.stopPropagation();
-            if( modal.classList.contains('--opened') && e.key === "Escape") {
-                modal.classList.remove('--opened')
+            if( modalRuler.classList.contains('--opened') && e.key === "Escape") {
+                modalRuler.classList.remove('--opened')
+                document.body.classList.remove('--no-scroll')
+            }
+            if( modalSize.classList.contains('--opened') && e.key === "Escape") {
+                modalSize.classList.remove('--opened')
                 document.body.classList.remove('--no-scroll')
             }
         })
     },
     init() {
         const _this = this
-        if (this.modalContainer()) {
-            let canvas = document.createElement('canvas')
-
-            let _size = ((rings[this.startValue].mm * 0.1 * window.ppcm)).toFixed(2)
-            const size =  _size + 'px';
-            const ctx = canvas.getContext('2d');
-            canvas.style.width = size;
-            canvas.style.height = size;
-
-            ctx.fillStyle = "#bada55";
-            ctx.fillRect(0, 0, 300, 300);
-            const imageElement  = document.querySelector("#image");
-            // imageElement.style.fontSize = rings[this.startValue].mm * 0.1 * window.ppcm +'px'
-            imageElement.style.width  = (rings[this.startValue].mm * 0.1).toFixed(2) * window.ppcm +  'px'
-            imageElement.style.height = (rings[this.startValue].mm * 0.1).toFixed(2) * window.ppcm +  'px'         
-            imageElement.src = canvas.toDataURL("image/png");
-
-            _this.setDefaultValue();
+        if (this.modalSizer()) {      
+            
             _this.rangebar();
             _this.close();
             _this.open();
