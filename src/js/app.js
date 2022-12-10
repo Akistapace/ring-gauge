2b1cb4915076e70932f781b736012b59b2030865
Fernando Aquistapace <fernando.akistapace@gmail.com>
2022-12-10 16:27:41 -0300

    .

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,18 @@
 // import listRings from "./list-rings";
 // import {modal} from "./modals";
-import {header} from "./header";
+import { header } from "./header";
 import { resizer } from "./resizer";
 import { handlePPIRule } from "./ruler";
+import { tabela } from "./tabela";
 
 (async ()=> {
     const page = document.body.classList
-    header.init()
+    header.init();
+
+    if (page.contains('barbante') || page.contains('regua')) {
+        tabela.init();
+    }
+
     if (page.contains('home')) {
         resizer.init();
         handlePPIRule.init();
---
+++ src/js/tabela.js
@@ -0,0 +1,71 @@
+import rings from "../data/rings.json";
+
+export const tabela = {
+    array_chunk(arr, len) {
+        let chunks = [], i = 0, n = arr.length
+        while (i < n) {
+            chunks.push(arr.slice(i, i += len))
+        }
+        return chunks      
+    },
+    mountTable() {
+        if (window.innerWidth > 768) {
+            let arr = this.array_chunk(rings, 18)
+            arr.forEach(item => {
+                const template = /*html*/ `
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Tamanho</th>
+                            <th>Milímetro</th>
+                            <th>Circunferrência</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${ item.map((e)=> {
+                            return `
+                                <tr>
+                                    <td>${e.size}</td>
+                                    <td>${e.mm}</td>
+                                    <td>${e.circunference}</td>
+                                </tr>
+                            `
+                            }).toString().replace(/,/g, '')
+                        }
+                    </tbody>
+                </table>
+            `;
+            document.querySelector('#tabela').insertAdjacentHTML('beforeend', template)
+            });
+        } else {
+            const template = /*html*/ `
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Tamanho</th>
+                            <th>Milímetro</th>
+                            <th>Circunferrência</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${ rings.map((e)=> {
+                            return `
+                                <tr>
+                                    <td>${e.size}</td>
+                                    <td>${e.mm}</td>
+                                    <td>${e.circunference}</td>
+                                </tr>
+                            `
+                            }).toString().replace(/,/g, '')
+                        }
+                    </tbody>
+                </table>
+            `;
+            document.querySelector('#tabela').insertAdjacentHTML('beforeend', template)
+        }
+
+    },
+    init() {
+        this.mountTable()
+    }
+}
